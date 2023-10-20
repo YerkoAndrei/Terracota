b99eda26d02205260ba1f1e501eb1de5efb275ba
Yerko Andrei <yerkoandrei@gmail.com>
2023-10-20 11:58:36 -0300

    rotador universal

---
+++ Terracota/Visuales/Rotador.cs
@@ -0,0 +1,14 @@
+﻿using Stride.Engine;
+using Stride.Core.Mathematics;
+
+namespace Terracota;
+
+public class Rotador : SyncScript
+{
+    public float ánguloY;
+
+    public override void Update()
+    {
+        Entity.Transform.Rotation *= Quaternion.RotationY(ánguloY / 100);
+    }
+}
