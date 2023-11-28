2f43c1d37b64e752d1747c13a2791c6fec58475c
Yerko Andrei <yerkoandrei@gmail.com>
2023-11-28 19:45:57 -0300

    fps promedio

---
+++ Terracota/Sistemas/ControladorInfo.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Stride.Engine;
+using Stride.UI;
+using Stride.UI.Controls;
+
+namespace Terracota;
+
+public class ControladorInfo : AsyncScript
+{
+    private TextBlock txtFPS;
+    private TextBlock txtPing;
+
+    public override async Task Execute()
+    {
+        var página = Entity.Get<UIComponent>().Page.RootElement;
+        txtFPS = página.FindVisualChildOfType<TextBlock>("txtFPS");
+        txtPing = página.FindVisualChildOfType<TextBlock>("txtPing");
+
+        var contador = 0;
+        var cuadros = 0f;
+
+        // Promedio de FPS cada 60 frames
+        while (Game.IsRunning)
+        {
+            // FPS
+            if (contador >= 60)
+            {
+                var fps = contador / cuadros;
+                txtFPS.Text = string.Format("FPS: {0}", fps.ToString("00"));
+                cuadros = 0;
+                contador = 0;
+            }
+            else
+            {
+                cuadros += (float)Game.UpdateTime.Elapsed.TotalSeconds;
+                contador++;
+            }
+
+            // PING
+            txtPing.Text = string.Format("Ping: {0}", 0);
+
+            await Script.NextFrame();
+        }
+    }
+}
