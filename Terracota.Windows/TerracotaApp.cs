4c5f00e8a4eaff3cb9fb9e1655a26fbfdcc7cb21
Yerko Andrei <yerkoandrei@gmail.com>
2024-06-24 21:37:44 -0400

    linux primer intento

--- Terracota.Windows/TerracotaApp.cs
+++ Terracota.Windows/TerracotaApp.cs
@@ -1,33 +1,32 @@
 using Stride.Engine;
 using System.Windows;
 
-namespace Terracota
+namespace Terracota;
+
+class TerracotaApp
 {
-    class TerracotaApp
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        using (var game = new Game())
         {
-            using (var game = new Game())
+            var vSync = false;
+
+            // Primer inicio
+            if (!SistemaMemoria.ObtenerExistenciaArchivo())
             {
-                var vSync = false;
-
-                // Primer inicio
-                if (!SistemaMemoria.ObtenerExistenciaArchivo())
-                {
-                    // 63 = barra de título
-                    var alto = (int)SystemParameters.FullPrimaryScreenHeight + 63;
-                    var ancho = (int)SystemParameters.FullPrimaryScreenWidth;
-                    SistemaMemoria.EstablecerConfiguraciónPredeterminada(ancho, alto);
-                }
-                else
-                    vSync = bool.Parse(SistemaMemoria.ObtenerConfiguración(Constantes.Configuraciones.vSync));
-
-                // vSync
-                game.IsDrawDesynchronized = !vSync;
-                game.GraphicsDeviceManager.SynchronizeWithVerticalRetrace = vSync;
-
-                game.Run();
+                // 63 = barra de título windows
+                var alto = (int)SystemParameters.FullPrimaryScreenHeight + 63;
+                var ancho = (int)SystemParameters.FullPrimaryScreenWidth;
+                SistemaMemoria.EstablecerConfiguraciónPredeterminada(ancho, alto);
             }
+            else
+                vSync = bool.Parse(SistemaMemoria.ObtenerConfiguración(Constantes.Configuraciones.vSync));
+
+            // vSync
+            game.IsDrawDesynchronized = !vSync;
+            game.GraphicsDeviceManager.SynchronizeWithVerticalRetrace = vSync;
+
+            game.Run();
         }
     }
 }
